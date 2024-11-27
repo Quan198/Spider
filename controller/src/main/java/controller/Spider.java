6a03ed03fff56df19839058a7bfbe4d293459805
yaozhih <yaozhh@ruit.cc>
2024-11-27 20:20:42 +0800

    添加项目结构

---
+++ controller/src/main/java/controller/Spider.java
@@ -0,0 +1,15 @@
+package controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author w
+ */
+@SpringBootApplication
+public class Spider {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spider.class, args);
+    }
+}
